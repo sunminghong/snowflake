d72b8e7b2d7d0f6062fb7f6e3e7562d0d6ee4ea2
allen <allen.fantasy@gmail.com>
2019-03-27 09:29:19 +0800

    add GetPrivateIP

---
+++ getlocalip.go
@@ -0,0 +1,40 @@
+package snowflake
+
+import "net"
+import "errors"
+
+
+func privateIPv4() (net.IP, error) {
+	as, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range as {
+		ipnet, ok := a.(*net.IPNet)
+		if !ok || ipnet.IP.IsLoopback() {
+			continue
+		}
+
+		ip := ipnet.IP.To4()
+		if isPrivateIPv4(ip) {
+			return ip, nil
+		}
+	}
+	return nil, errors.New("no private ip address")
+}
+
+func isPrivateIPv4(ip net.IP) bool {
+	return ip != nil &&
+		(ip[0] == 10 || ip[0] == 172 && (ip[1] >= 16 && ip[1] < 32) || ip[0] == 192 && ip[1] == 168)
+}
+
+func GetPrivateIP() uint16 {
+	ip, err := privateIPv4()
+	if err != nil {
+		return 0
+	}
+
+	return uint16(ip[2])<<8 + uint16(ip[3])
+}
+
